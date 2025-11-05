cee88e8bbfdb99d176987562c9795011f514f031
f1nch <yarvar03@gmail.com>
2025-11-05 11:56:05 -0800

    Реализованны функции encrypt_caesar() и decrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -12,10 +12,10 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     ciphertext = ""
     for ch in plaintext:
-        if 'A' <= ch <= 'Z':
-            ciphertext += chr((ord(ch) - ord('A') + shift) % 26 + ord('A'))
-        elif 'a' <= ch <= 'z':
-            ciphertext += chr((ord(ch) - ord('a') + shift) % 26 + ord('a'))
+        if "A" <= ch <= "Z":
+            ciphertext += chr((ord(ch) - ord("A") + shift) % 26 + ord("A"))
+        elif "a" <= ch <= "z":
+            ciphertext += chr((ord(ch) - ord("a") + shift) % 26 + ord("a"))
         else:
             ciphertext += ch
     return ciphertext
@@ -35,10 +35,10 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     plaintext = ""
     for ch in ciphertext:
-        if 'A' <= ch <= 'Z':
-            plaintext += chr((ord(ch) - ord('A') - shift) % 26 + ord('A'))
-        elif 'a' <= ch <= 'z':
-            plaintext += chr((ord(ch) - ord('a') - shift) % 26 + ord('a'))
+        if "A" <= ch <= "Z":
+            plaintext += chr((ord(ch) - ord("A") - shift) % 26 + ord("A"))
+        elif "a" <= ch <= "z":
+            plaintext += chr((ord(ch) - ord("a") - shift) % 26 + ord("a"))
         else:
             plaintext += ch
     return plaintext
