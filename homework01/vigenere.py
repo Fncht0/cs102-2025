6f6cb3e65a7551febb72f49db92453138f24ad1e
f1nch <yarvar03@gmail.com>
2025-11-04 23:23:24 -0800

    Реализована функция encrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,7 +9,20 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    keyword = (keyword * (len(plaintext) // len(keyword) + 1))[:len(plaintext)]
+
+    for i in range(len(plaintext)):
+        p = plaintext[i]
+        k = keyword[i]
+
+        if p.isalpha():
+            shift = ord(k.lower()) - ord('a')
+            if p.isupper():
+                ciphertext += chr((ord(p) - ord('A') + shift) % 26 + ord('A'))
+            else:
+                ciphertext += chr((ord(p) - ord('a') + shift) % 26 + ord('a'))
+        else:
+            ciphertext += p
     return ciphertext
 
 
@@ -24,5 +37,18 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    keyword = (keyword * (len(ciphertext) // len(keyword) + 1))[:len(ciphertext)]
+
+    for i in range(len(ciphertext)):
+        c = ciphertext[i]
+        k = keyword[i]
+
+        if c.isalpha():
+            shift = ord(k.lower()) - ord('a')
+            if c.isupper():
+                plaintext += chr((ord(c) - ord('A') - shift) % 26 + ord('A'))
+            else:
+                plaintext += chr((ord(c) - ord('a') - shift) % 26 + ord('a'))
+        else:
+            plaintext += c
     return plaintext
