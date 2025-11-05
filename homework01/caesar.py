064336c49a48661cbdf5fb720b1ed653986fe7db
f1nch <yarvar03@gmail.com>
2025-11-04 20:20:54 -0800

    Реализована функция encrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -11,7 +11,13 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for ch in plaintext:
+        if 'A' <= ch <= 'Z':
+            ciphertext += chr((ord(ch) - ord('A') + shift) % 26 + ord('A'))
+        elif 'a' <= ch <= 'z':
+            ciphertext += chr((ord(ch) - ord('a') + shift) % 26 + ord('a'))
+        else:
+            ciphertext += ch
     return ciphertext
 
 
@@ -28,5 +34,11 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for ch in ciphertext:
+        if 'A' <= ch <= 'Z':
+            plaintext += chr((ord(ch) - ord('A') - shift) % 26 + ord('A'))
+        elif 'a' <= ch <= 'z':
+            plaintext += chr((ord(ch) - ord('a') - shift) % 26 + ord('a'))
+        else:
+            plaintext += ch
     return plaintext
