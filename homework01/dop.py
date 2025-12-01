d548c5b7b268b78dad79afe4e3aa80ec6e2be85a
f1nch <yarvar03@gmail.com>
2025-12-01 13:01:24 -0800

    Исправлен код дополнительной задачи

--- homework01/dop.py
+++ homework01/dop.py
@@ -1,18 +1,19 @@
-def decrypt_poly_shift(ciphertext, odd_shift, even_shift):
+def decrypt_poly_shift(ciphertext: str, odd_shift: int, even_shift: int) -> str:
     alph_up = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
     alph_low = alph_up.lower()
-    plaintext = ""
+    n = len(alph_up)
+    plaintext = []
+
     for i, ch in enumerate(ciphertext, start=1):
         if ch in alph_up:
-            alph = alph_up
-            shift = odd_shift if i % 2 == 1 else even_shift
-            new_index = (alph.index(ch) - shift) % len(alph)
-            plaintext += alph[new_index]
+            shift = odd_shift if (i % 2 == 1) else even_shift
+            new_index = (alph_up.index(ch) - shift) % n
+            plaintext.append(alph_up[new_index])
         elif ch in alph_low:
-            alph = alph_low
-            shift = odd_shift if i % 2 == 1 else even_shift
-            new_index = (alph.index(ch) - shift) % len(alph)
-            plaintext += alph[new_index]
+            shift = odd_shift if (i % 2 == 1) else even_shift
+            new_index = (alph_low.index(ch) - shift) % n
+            plaintext.append(alph_low[new_index])
         else:
-            plaintext += ch
-        return plaintext
+            plaintext.append(ch)
+
+    return "".join(plaintext)
